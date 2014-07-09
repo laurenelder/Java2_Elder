75b504bde194889496327b5051ef6148da2dbbb7
Lauren Elder <laurenelder@fullsail.edu>
2014-07-09 19:35:37 -0400

    Added Basic UI

--- MovieLookup/gen/com/laurenelder/movielookup/R.java
+++ MovieLookup/gen/com/laurenelder/movielookup/R.java
@@ -25,8 +25,12 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080001;
+        public static final int action_settings=0x7f080005;
         public static final int container=0x7f080000;
+        public static final int findButton=0x7f080003;
+        public static final int listView1=0x7f080004;
+        public static final int searchField=0x7f080002;
+        public static final int textView1=0x7f080001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -38,7 +42,10 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050002;
         public static final int app_name=0x7f050000;
+        public static final int button_text=0x7f050004;
         public static final int hello_world=0x7f050001;
+        public static final int search_hint=0x7f050005;
+        public static final int title_text=0x7f050003;
     }
     public static final class style {
         /** 

c180ef02134c9d728fe52825ea28faa89f8ff080
Lauren Elder <laurenelder@fullsail.edu>
2014-07-10 19:14:40 -0400

    Added Connection, Parse, and Custom ListView

--- MovieLookup/gen/com/laurenelder/movielookup/R.java
+++ MovieLookup/gen/com/laurenelder/movielookup/R.java
@@ -25,16 +25,20 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080005;
+        public static final int action_settings=0x7f080008;
         public static final int container=0x7f080000;
         public static final int findButton=0x7f080003;
-        public static final int listView1=0x7f080004;
+        public static final int list=0x7f080004;
+        public static final int movieTitle=0x7f080005;
+        public static final int movieType=0x7f080007;
+        public static final int movieYear=0x7f080006;
         public static final int searchField=0x7f080002;
         public static final int textView1=0x7f080001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int fragment_main=0x7f030001;
+        public static final int custom_list_item=0x7f030001;
+        public static final int fragment_main=0x7f030002;
     }
     public static final class menu {
         public static final int main=0x7f070000;
--- MovieLookup/src/com/laurenelder/movielookup/MainActivity.java
+++ MovieLookup/src/com/laurenelder/movielookup/MainActivity.java
@@ -8,12 +8,24 @@
 
 package com.laurenelder.movielookup;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.app.Activity;
 import android.app.ActionBar;
 import android.app.AlertDialog;
 import android.app.Fragment;
 import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -25,8 +37,13 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.os.Build;
 
@@ -36,7 +53,13 @@ public class MainActivity extends Activity {
 	static String tag = "MAINACTIVITY";
 	EditText searchField;
 	Button findButton;
-	Context context;
+	static Context context;
+	
+	// Array Adapter
+	ArrayAdapter<MovieListItems> listAdapter;
+	
+	// Class List
+	List<MovieListItems> movieList = new ArrayList<MovieListItems>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,26 +89,46 @@ public class MainActivity extends Activity {
 							// TODO Auto-generated method stub
 							if (msg.arg1 == RESULT_OK) {
 								Log.i(tag, msg.obj.toString());
+								parseData(msg.obj.toString());
 								// Check to make sure raw JSON was saved
 								// Run parse method and save parsed data
 								// Update UI with parsed data
 							}
 						}
 					};
-					Messenger apiMessenger = new Messenger(apiHandler);
 					
-					String myURL = getResources().getString(R.string.main_api);
-					myURL = myURL.replace("_", "&");
-					
-					Intent startApiIntent = new Intent(context, ApiService.class);
-					startApiIntent.putExtra(ApiService.MESSENGER_KEY, apiMessenger);
-					startApiIntent.putExtra(ApiService.INPUT_KEY, myURL);
-					startService(startApiIntent);
-					showNotfication("searching");
+					if (checkNetworkConnection(context)) {
+						Messenger apiMessenger = new Messenger(apiHandler);
+						
+						String myURL = getResources().getString(R.string.main_api);
+						myURL = myURL.replace("_", "&");
+						
+						Intent startApiIntent = new Intent(context, ApiService.class);
+						startApiIntent.putExtra(ApiService.MESSENGER_KEY, apiMessenger);
+						startApiIntent.putExtra(ApiService.INPUT_KEY, myURL);
+						startService(startApiIntent);
+						showNotfication("searching");
+					} else {
+						showNotfication("connection");
+					}
 				}
 			}
-        	
         });
+        
+        // ListView Code 
+		listAdapter = new customListAdapter();
+        ListView listView = (ListView)findViewById(R.id.list);
+		listView.setAdapter(listAdapter);
+		listView.setOnItemClickListener(new OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view,
+					int position, long id) {
+				
+				// Load Movie Details UI here
+			}
+
+		});
     }
 
     @Override
@@ -138,5 +181,98 @@ public class MainActivity extends Activity {
     		Toast.makeText(this, R.string.searching_notification, Toast.LENGTH_LONG).show();
     	}
     }
+	
+	// Get and Parse JSON Function
+	public void parseData(String apiData) {
 
+		String jsonString = null;
+		if (apiData != null) {
+			jsonString = apiData;
+		}
+
+		// Parse JSON
+		try {
+			// Creating JSONObject from String
+				JSONObject mainObject = new JSONObject(jsonString);
+				JSONArray subObject = mainObject.getJSONArray("Search");
+				Log.i(tag, subObject.toString());
+
+				for (int i = 0; i < subObject.length(); i ++) {
+					JSONObject movieObject = subObject.getJSONObject(i);
+					// Class Specific Data
+					
+					String thisName = movieObject.getString("Title");
+					String thisYear = movieObject.getString("Year");
+					String thisType = movieObject.getString("Type");
+					String thisID = movieObject.getString("imdbID");
+					Log.i(tag, thisName);
+					Log.i(tag, thisYear);
+					Log.i(tag, thisType);
+					Log.i(tag, thisID);
+
+					// Save Data Here
+					setClass(thisName, thisYear, thisType, thisID);
+				}
+				listAdapter.notifyDataSetChanged();
+		} 
+		catch (JSONException e) {
+			// TODO Auto-generated catch block
+			Log.e(tag, e.getMessage().toString());
+			e.printStackTrace();
+		}
+	}
+
+	// Save Parsed Data to Class
+	public void setClass(String name, String year, String type, String ID) {
+		MovieListItems newMovie = new MovieListItems(name, year, type, ID);
+		movieList.add(newMovie);
+		Log.i(tag, newMovie.toString());
+	}
+	
+    // Check Network Connection Method
+    public Boolean checkNetworkConnection (Context context) {
+    	Boolean connected = false;
+    	ConnectivityManager connManag = (ConnectivityManager) MainActivity.context
+    			.getSystemService
+    			(Context.CONNECTIVITY_SERVICE);
+    	NetworkInfo networkInfomation = connManag.getActiveNetworkInfo();
+    	if (networkInfomation != null) {
+    		if (networkInfomation.isConnected()) {
+    			Log.i(tag, "Connection Type: " + networkInfomation.getTypeName()
+    					.toString());
+    			connected = true;
+    		}
+    	}
+    	return connected;
+    }
+    
+    // Custom ListAdapter Class
+    public class customListAdapter extends ArrayAdapter <MovieListItems> {
+    	public customListAdapter() {
+    		super(context, R.layout.custom_list_item, movieList);
+    	}
+
+    	// Set List Item Information
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			View customItemView = convertView;
+			
+			Log.i(tag, "Custom Adapter Hit");
+
+			if (customItemView == null) {
+				LayoutInflater viewInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+				customItemView = viewInflater.inflate(R.layout.custom_list_item, parent, false);
+			}
+
+			TextView name = (TextView)customItemView.findViewById(R.id.movieTitle);
+			TextView year = (TextView)customItemView.findViewById(R.id.movieYear);
+			TextView type = (TextView)customItemView.findViewById(R.id.movieType);
+
+			name.setText(movieList.get(position).movieName.toString());
+			year.setText(movieList.get(position).movieYear.toString());
+			type.setText(movieList.get(position).movieType.toString());
+
+			return customItemView;
+		}
+    }
 }

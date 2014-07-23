3754dde8e7707b271ba0953fa6fe4269c26b6378
Lauren Elder <laurenelder@fullsail.edu>
2014-07-23 12:15:32 -0400

    Added Base Fragment Classes

---
+++ MovieLookup/src/com/laurenelder/movielookup/DetailsFragment.java
@@ -0,0 +1,32 @@
+/*
+ * Project:			MovieLookup
+ * Package:			com.laurenelder.movielookup
+ * Author:			Devin "Lauren" Elder
+ * Date:			Jul 23, 2014
+ * Class:			Java 2 Term 1407
+ */
+package com.laurenelder.movielookup;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class DetailsFragment extends Fragment{
+
+	@Override
+	public void onAttach(Activity activity) {
+		// TODO Auto-generated method stub
+		super.onAttach(activity);
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		return super.onCreateView(inflater, container, savedInstanceState);
+	}
+
+}
---
+++ MovieLookup/src/com/laurenelder/movielookup/MainFragment.java
@@ -0,0 +1,15 @@
+/*
+ * Project:			MovieLookup
+ * Package:			com.laurenelder.movielookup
+ * Author:			Devin "Lauren" Elder
+ * Date:			Jul 23, 2014
+ * Class:			Java 2 Term 1407
+ */
+
+package com.laurenelder.movielookup;
+
+import android.app.Fragment;
+
+public class MainFragment extends Fragment{
+
+}
